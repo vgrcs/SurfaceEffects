edbf898207669a4fdb5ffd5fc1e49d7882e2647d
vgrcs@rit.edu <victor.mr.gabriel@gmail.com>
2023-07-12 11:35:13 +0100

    change the object of induction

--- MapFind.v
+++ MapFind.v
@@ -10,50 +10,48 @@ Require Import Top0.Keys.
 Require Import Top0.Definitions.
 Require Import Top0.Nameless.
 
-(*Module ST := FMapAVL.Make (RegionVars).
-Definition Sigma : Type := ST.t tau.
 
+Module STProofs := ST.Raw.Proofs.
 Module STMapP := FMapFacts.Facts ST.
-Module STRaw := ST.Raw.
-Module STProofs := ST.Raw.Proofs.*)
-Module EProofs := ST.Raw.Proofs.
+Module STMapProp := FMapFacts.Properties ST.
 
 Definition Functional_Map_Union (stty1 stty2 : Sigma) : Sigma :=
   let f := fun (k : nat * nat) (v : tau) (m : Sigma) => ST.add k v m
   in ST.fold f stty1 stty2.
 
 
-(*Lemma StoreTyping_Extended:
-  forall stty sttya sttyb,
-    (forall rho env ctxt, TcEnv (stty, rho, env, ctxt) ) ->
-    (forall (l : ST.key) (t' : tau),
-       ST.find (elt:=tau) l stty = Some t' -> ST.find (elt:=tau) l sttya = Some t' ) ->
-    (forall (l : ST.key) (t' : tau),
-       ST.find (elt:=tau) l stty = Some t' -> ST.find (elt:=tau) l sttyb = Some t' ) ->
-    (forall (l : ST.key) (t' : tau),
-    	ST.find (elt:=tau) l stty = Some t' -> ST.find (elt:=tau) l (Functional_Map_Union sttya sttyb) = Some t' ).
-Proof. 
-  intros stty sttya sttyb Ha Hb.
-  intros l t' H.
-  assert ((forall rho env ctxt, TcEnv (stty, rho, env, ctxt) -> TcEnv (sttya, rho, env, ctxt))) as HA.
-  apply ext_stores__env; auto.
-  assert ((forall rho env ctxt, TcEnv (stty, rho, env, ctxt) -> TcEnv (sttyb, rho, env, ctxt))) as HB.
-  apply ext_stores__env; auto.
-  assert (forall (rho : Rho) (env : Env) (ctxt : Gamma), TcEnv (sttya, rho, env, ctxt)) as HTCEnv_a
-       by (intros; apply HA; auto).
-  assert (forall (rho : Rho) (env : Env) (ctxt : Gamma), TcEnv (sttyb, rho, env, ctxt)) as HTCEnv_b
-       by (intros; apply HB; auto).
-  edestruct HTCEnv_a.
-  unfold ST.find in *.
-  intro.
-  apply EProofs.find_1 with (e:=H) (m:=ST.this (Functional_Map_Union sttya sttyb)) (x:=t').
-  edestruct (Ha l t' H).
-  generalize l. 
-  apply Functional_Map_Union_find.
-Admitted.*)
-
   
 Lemma Functional_Map_Union_find:
+  forall sttya sttyb (l : ST.key) (t' : tau),
+    ST.find (elt:=tau) l (Functional_Map_Union sttya sttyb) = ST.find (elt:=tau) l sttya.
+Proof.
+  intros.
+  unfold Functional_Map_Union. (*ST.fold.*) 
+  destruct sttya. destruct sttyb. simpl. induction this.
+  - inversion is_bst0; subst.
+    + unfold ST.fold. simpl.
+      reflexivity.
+    + unfold ST.fold. simpl. 
+      assert (STProofs.R_find tau x (ST.Raw.Leaf tau) None) by (eapply STProofs.R_find_0; auto).
+      (* wrong *)
+      admit.
+  - inversion is_bst; subst.
+     assert ( ST.find (elt:=tau) l
+                (ST.fold (fun (k0 : nat * nat) (v : tau) (m : Sigma) => ST.add k0 v m)
+                         {| ST.this := this1; ST.is_bst := H3|}
+                         {| ST.this := this0; ST.is_bst := is_bst0 |}) =
+                ST.find (elt:=tau) l {| ST.this := this1; ST.is_bst := H3 |}
+            ) as HA by (apply IHthis1).
+     assert ( ST.find (elt:=tau) l
+                (ST.fold (fun (k0 : nat * nat) (v : tau) (m : Sigma) => ST.add k0 v m)
+                         {| ST.this := this2; ST.is_bst := H5|}
+                         {| ST.this := this0; ST.is_bst := is_bst0 |}) =
+                ST.find (elt:=tau) l {| ST.this := this2; ST.is_bst := H5 |}
+            ) as HB by (apply IHthis2).
+Admitted.
+
+
+Lemma Functional_Map_Union_find_1:
   forall sttya sttyb (l : ST.key) (t' : tau),
     ST.find (elt:=tau) l (Functional_Map_Union sttya sttyb) = ST.find (elt:=tau) l sttya.
 Proof.
@@ -63,10 +61,10 @@ Proof.
   - inversion is_bst; subst.
     + reflexivity. 
     + unfold ST.find, ST.fold. simpl.
-      destruct ( RegionVars.compare l x); simpl.
+      destruct (RegionVars.compare l x).
       * admit.
-      * admit.
-      * admit.
+      * admit. (* should be true*)
+      * admit.            
   - inversion is_bst0; subst.
 
     assert (
